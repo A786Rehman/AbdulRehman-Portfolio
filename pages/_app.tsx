8c7fa280a8f30cd04e78f3084a94fd8bf971c6b1
Abdul Rehman <ab.rahmanedu@gmail.com>
2026-07-08 17:11:20 +0500

    Add Google Search Console verification meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
+// import Head from "next/head";
+// import "@/styles/globals.css";
+// import type { AppProps } from "next/app";
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
+
+import Head from "next/head";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="oDBjgx6N6rK4gEWaMQgnySGHCFKl_Nu0X3FgVvfIsao"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
